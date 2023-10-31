62f45a026e1733b5c59f20ddf55b04f7d1d4cfa7
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-11-01 02:43:16 +0530

    Added Migration to add new columns to booking service

--- src/index.js
+++ src/index.js
@@ -22,4 +22,4 @@ const setupAndStartServer = () => {
     })
 }
 
-setupAndStartServer(); 
+setupAndStartServer();   
--- src/models/booking.js
+++ src/models/booking.js
@@ -27,7 +27,17 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       values: ['InProcess', 'Booked', 'Cancelled'],
       defaultValue: 'InProcess'
-    }  
+    },
+    noOfSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
+    totalCost: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
   }, {
     sequelize,
     modelName: 'Booking',
