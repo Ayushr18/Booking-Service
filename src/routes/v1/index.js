77ac5637967eac356e336892ffb7959fadfa1384
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-11-13 13:03:45 +0530

    Added rabbit mq config

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
-const {BookingController} = require('../../controllers/index');
-
+const {BookingController }= require('../../controllers/index');
+// const {createChannel} = require('../../utils/messageQueue');
+// const channel = await createChannel();
+const bookingController = new BookingController();
 const router = express.Router();
 
-router.post('/bookings', BookingController.create); 
+router.post('/bookings', bookingController.create); 
+router.post('/public', bookingController.sendMessageToQueue);
 
 module.exports = router; 
